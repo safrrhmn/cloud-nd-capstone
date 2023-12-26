06ee43a261cb671dc9fedd30ac5c3ee6ff87bfd0
Saifur Rahman <safrrhmn@hotmail.com>
2023-12-26 10:31:00 -0500

    added dependencies

---
+++ src/main/java/com/saifurtech/locale/LocaleApplication.java
@@ -0,0 +1,13 @@
+package com.saifurtech.locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saifurtech/locale/LocaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saifurtech.locale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
